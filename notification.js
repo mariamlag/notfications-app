3ce36b70f8d4ff48344e4b1b04aadaa37d54f29a
mariamlag <114602082+mariamlag@users.noreply.github.com>
2023-04-07 23:20:01 +0400

    last

---
+++ notification.js
@@ -0,0 +1,23 @@
+const collection = document.getElementById("container").children;
+const points = document.querySelectorAll(".point");
+const same = document.querySelectorAll(".same");
+const mark = document.getElementById("mark");
+const sum = document.getElementById("number");
+let count = parseInt(sum.textContent);
+//all element remove background and style, also notification number = 0
+mark.addEventListener("click", () => {
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.backgroundColor = "white";
+            sum.textContent = "0";
+            points[i].style.display = "none";
+        }
+});
+//all element which is new notification make read, when I click some notification this background and red point remove 
+for(let i = 0; same.length; i++){  
+    same[i].addEventListener("click", () => {
+        same[i].style.backgroundColor = "white"; 
+        points[i].style.display = "none";
+        count--;
+        sum.textContent = count.toString();
+    });
+};
